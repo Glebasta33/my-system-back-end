1aa1cde45e79031a739e5da186407da6e6dfd714
Gleb Trusov <gleb.trusow@yandex.ru>
2024-03-21 11:19:55 +0300

    Добавил сериализацию

--- src/main/kotlin/com/mysystem/plugins/Routing.kt
+++ src/main/kotlin/com/mysystem/plugins/Routing.kt
@@ -3,11 +3,20 @@ package com.mysystem.plugins
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
 
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
         }
+        get("/serialization") {
+            call.respond(SerializationTest("Hello, serialization"))
+        }
     }
 }
+
+@Serializable
+data class SerializationTest(
+    val text: String
+)
