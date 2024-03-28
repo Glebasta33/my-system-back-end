c0019494390a74151e60fdde9815325aae8ecb93
Gleb Trusov <gleb.trusow@yandex.ru>
2024-03-28 13:32:11 +0300

    Добавил новый тестовый путь

--- src/main/kotlin/com/mysystem/plugins/Routing.kt
+++ src/main/kotlin/com/mysystem/plugins/Routing.kt
@@ -13,10 +13,30 @@ fun Application.configureRouting() {
         get("/serialization") {
             call.respond(SerializationTest("Hello, serialization"))
         }
+        get("/tasks") {
+            call.respond(
+                listOf(
+                    Task(
+                        id = "id_1",
+                        name = "Купить молока"
+                    ),
+                    Task(
+                        id = "id_2",
+                        name = "Изучить основы компьютерных сетей"
+                    )
+                )
+            )
+        }
     }
 }
 
 @Serializable
 data class SerializationTest(
     val text: String
+)
+
+@Serializable
+data class Task(
+    val id: String,
+    val name: String
 )
