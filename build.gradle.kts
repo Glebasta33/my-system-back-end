bd79d8caa1fe19e8bc15ac05ec88e712d53f2f5b
Gleb Trusov <gleb.trusow@yandex.ru>
2024-04-03 10:52:15 +0300

    Добавил зависимости Exposed и PostgreSQL

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val exposed_version = "0.48.0"
 
 plugins {
     kotlin("jvm") version "1.9.23"
@@ -32,4 +33,10 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    // БД:
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.postgresql:postgresql:42.7.0")
 }
--- src/main/kotlin/com/mysystem/Application.kt
+++ src/main/kotlin/com/mysystem/Application.kt
@@ -5,8 +5,15 @@ import com.mysystem.plugins.configureSerialization
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import org.jetbrains.exposed.sql.Database
 
 fun main() {
+     Database.connect(
+         url = "jdbc:postgresql://localhost:5432/mobile_developer_course",
+         driver = "org.postgresql.Driver",
+         password = "admin"
+     )
+
     embeddedServer(
         Netty,
         port = 80,
