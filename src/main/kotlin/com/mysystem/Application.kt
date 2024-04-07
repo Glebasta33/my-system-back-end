22d834585f94bebeb59cbf2af6b7b3514949b257
Gleb Trusov <gleb.trusow@yandex.ru>
2024-04-07 11:47:05 +0300

    Добавил настройки для подключения к удалённой БД PostgreSQL

--- src/main/kotlin/com/mysystem/Application.kt
+++ src/main/kotlin/com/mysystem/Application.kt
@@ -1,24 +1,34 @@
 package com.mysystem
 
+import com.mysystem.database.tasks.TasksTable
 import com.mysystem.plugins.configureRouting
 import com.mysystem.plugins.configureSerialization
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main() {
-     Database.connect(
-         url = "jdbc:postgresql://localhost:5432/mobile_developer_course",
-         driver = "org.postgresql.Driver",
-         password = "admin"
-     )
+    initDB()
 
     embeddedServer(
         Netty,
         port = 80,
         module = Application::module
     ).start(wait = true)
+}
+
+private fun initDB() {
+    Database.connect(
+        url = "jdbc:postgresql://62.113.115.167/test_db",
+        driver = "org.postgresql.Driver",
+        user = "postgres",
+        password = "admin"
+    )
+
+    transaction { SchemaUtils.create(TasksTable) }
 }
 
 fun Application.module() {
